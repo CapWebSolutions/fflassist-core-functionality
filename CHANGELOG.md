7b8802edbe5b38d81e8da197c274693a0b2557f9
Matt Ryan <msr1222@gmail.com>
2025-01-28 12:30:06 -0500

    Updates to get_plugin data order

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -5,6 +5,12 @@ All notable changes to this project will be documented in this file.
 The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.1.0/),
 and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0.html).
 
+## [1.1.3] - 2025-01-28
+
+### Changed
+
+- Adjusted styling on shortcode button for login / logout so that it mimics the theme inherited style of buttons.
+
 ## [1.1.2] - 2025-01-01
 
 ### Fixed
--- lib/functions/tweak-pmpro.php
+++ lib/functions/tweak-pmpro.php
@@ -25,7 +25,7 @@ function shortcode_forstuff() {
     <?php if (is_user_logged_in()) : ?>
         <div class="kt-inside-inner-col">
             <div class="wp-block-kadence-advancedbtn kb-buttons-wrap">
-                <a class="kb-button kt-button button kt-btn-size-standard kt-btn-width-type-auto kb-btn-global-fill kt-btn-has-text-true kt-btn-has-svg-false wp-block-kadence-singlebtn" href="<?php echo wp_logout_url( home_url() ); ?>">
+                <a class="kb-button kt-button button kt-btn-size-standard kt-btn-width-type-auto kb-btn-global-inherit kt-btn-has-text-true kt-btn-has-svg-false wp-block-button__link wp-block-kadence-singlebtn" href="<?php echo wp_logout_url( home_url() ); ?>">
                     <span class="kt-btn-inner-text">User logged in. Click to log out.</span>
                 </a>
             </div>
