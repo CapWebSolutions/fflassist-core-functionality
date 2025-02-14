e25fa2ebe91f01f02fb26674440f2a645ad2d1ed
Matt Ryan <msr1222@gmail.com>
2025-02-14 17:08:31 -0500

    Tweak custom user fields PMPro

---
+++ lib/functions/tweak-pmpro-custom-user-fields.php
@@ -0,0 +1,21 @@
+<?php 
+/**
+ * Tweak PMPro Custom User Fields
+ *
+ * This file contains any functions that tweak the PMPro Custom User Fields
+ *
+ * @package      FFL_Assist_Core_Functionality
+ * @since        1.0.0
+ * @link
+ */
+
+namespace capweb;
+
+function my_pmpro_get_user_meta($user_id){
+    // Don't break if PMPro is out of date or not loaded.
+	if ( ! function_exists( 'pmpro_add_user_field' ) ) {
+		return false;
+	}
+}
+add_action( 'init', __NAMESPACE__ . '\my_pmpro_get_user_meta' );
+
