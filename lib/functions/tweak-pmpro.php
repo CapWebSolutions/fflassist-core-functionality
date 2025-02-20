3f74429383e0b551c3927e1e4c87d31d90a307f0
Matt Ryan <msr1222@gmail.com>
2025-02-20 16:06:52 -0500

    Redirect Demo users to My account page on logout;

--- lib/functions/tweak-pmpro.php
+++ lib/functions/tweak-pmpro.php
@@ -214,4 +214,33 @@ function my_pmpro_login_redirect_url( $redirect_to, $request, $user ) {
 // add_filter( 'pmpro_login_redirect_url','my_pmpro_login_redirect_url', 10, 3 );
 
 
+/**
+ * Redirect members and non-members on logout to specified pages
+ * Learn more at https://www.paidmembershipspro.com/redirect-logout/
+ *
+ * title: Redirect member on logout
+ * layout: snippet
+ * collection: misc
+ * category: logout, redirect
+ *
+ * You can add this recipe to your site by creating a custom plugin
+ * or using the Code Snippets plugin available for free in the WordPress repository.
+ * Read this companion article for step-by-step directions on either method.
+ * https://www.paidmembershipspro.com/create-a-plugin-for-pmpro-customizations/
+ */
 
+ function my_pmpro_redirect_members_on_logout( $user_id ) {
+	if ( function_exists( 'pmpro_hasMembershipLevel' ) ) {
+		error_log( 'Running lo redirect ' . var_export( $user_id, true ) );
+		// Redirect users with membership level 1 to /level-1 page
+		if ( pmpro_hasMembershipLevel( 1, $user_id ) ) {
+			wp_redirect( '/my-account' );
+			exit;
+		} else {
+			// Redirect non-members and members with other levels to /other-levels page.
+			wp_redirect( '/' );
+			exit;
+		}
+	}
+}
+add_action( 'wp_logout', 'my_pmpro_redirect_members_on_logout' );
